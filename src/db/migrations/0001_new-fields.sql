00a681cc0d937a880da39b9d2d49ffb47ba3fda1
Celso Pires <celso.pires1999@gmail.com>
2025-01-10 18:11:38 -0300

    chore: Streamlined project organization for enhanced workflow efficiency

--- src/db/migrations/0001_new-fields.sql
+++ src/db/migrations/0001_new-fields.sql
@@ -6,8 +6,8 @@ ALTER TABLE "agreements" ADD COLUMN "is_revised" boolean DEFAULT false;
 ALTER TABLE "services" ADD COLUMN "provider_allocation" text;
 ALTER TABLE "services" ADD COLUMN "local_allocation" text;
 
-UPDATE agreements SET code = substr(name, 1, 20);
-UPDATE services SET provider_allocation = 'to be defined', local_allocation = 'to be defined';
+-- UPDATE agreements SET code = substr(name, 1, 20);
+-- UPDATE services SET provider_allocation = 'to be defined', local_allocation = 'to be defined';
 
 ALTER TABLE "agreements" ALTER COLUMN "code" SET NOT NULL;
 ALTER TABLE "agreements" ALTER COLUMN "is_revised" SET NOT NULL;
