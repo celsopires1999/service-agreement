32263fb68a4927f6d6ed674d192ad40ec53caba6
Celso Pires <celso.pires1999@gmail.com>
2025-12-29 21:27:31 +0000

    chore: update dev environment and project configuration
    - Optimize Docker start script to skip 'npm ci' if node_modules exists.
    - Update .prettierrc to include JSON files in overrides.
    - Adjust docker-compose.override.example.yaml (remove AUTH_URL, add volumes comment).
    - Remove unused 'deploy' script from package.json.

--- .docker/start.sh
+++ .docker/start.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-echo "Installing dependencies..."
-npm ci
+if [ ! -d "node_modules" ]; then
+    echo "node_modules not found. Instaling dependencies..."
+    npm ci
+else
+    echo "node_modules exists already."
+fi
 
 echo "App is ready."
 tail -f /dev/null
